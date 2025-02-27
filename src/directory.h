931d6095b3ba9128500eb54ab215ccb961df7c33
Matias Raimondi <matiraimondi1@gmail.com>
2025-02-27 20:36:16 -0300

    Define Directory Class

---
+++ src/directory.h
@@ -0,0 +1,25 @@
+#ifndef DIRECTORY_H
+#define DIRECTORY_H
+
+#include "contact.h"
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+class Directory {
+    private:
+        vector<Contact> contacts;
+
+        void saveContacts();
+        void loadContacts();
+
+    public:
+        Directory();
+
+        void addContact();
+        void showContacts() const;
+        void searchContact() const;
+        void removeContact();
+};
+
+#endif

bb78a9d7b2b3079c95703732da529bfe1047ad73
Matias Raimondi <matiraimondi1@gmail.com>
2025-02-28 19:20:21 -0300

    Added documentation for 'directory.h'

--- src/directory.h
+++ src/directory.h
@@ -1,3 +1,6 @@
+/// @file directory.h
+/// @brief Defines the Directory class, which manages a list of contacts.
+
 #ifndef DIRECTORY_H
 #define DIRECTORY_H
 
@@ -6,19 +9,48 @@
 #include <fstream>
 #include <sstream>
 
+/**
+ * @class Directory
+ * @brief Manages a collection of contacts.
+ */
 class Directory {
     private:
-        vector<Contact> contacts;
+        vector<Contact> contacts; ///< List of contacts.
 
+        /**
+        * @brief Saves the contacts to a file.
+        */
         void saveContacts();
+
+        /**
+        * @brief Loads contacts from a file.
+        */
         void loadContacts();
 
     public:
+        /**
+        * @brief Constructor for Directory.
+        */
         Directory();
 
+        /**
+        * @brief Adds a new contact to the directory.
+        */
         void addContact();
+
+        /**
+        * @brief Displays all contacts stored in the directory.
+        */
         void showContacts() const;
+
+        /**
+        * @brief Searches for a contact by name.
+        */
         void searchContact() const;
+
+        /**
+        * @brief Removes a contact from the directory.
+        */
         void removeContact();
 };
 
